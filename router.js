9e634f654378d89c3d65e69b2b39d8861bed26be
Finnedorn <daniele.giuntoli@outlook.com>
2024-05-24 14:17:50 +0200

    added basic route "/" settings

--- router.js
+++ router.js
@@ -2,3 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.get("/",(req, res) =>{
+    res.end("<h1>Benvenut* nel mio blog!</h1>")
+});
